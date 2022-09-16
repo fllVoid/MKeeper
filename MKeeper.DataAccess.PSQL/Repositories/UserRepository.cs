6d01499238eb30d18e65955634db06dc5a069854
Ni K <barneyfrommesa@gmail.com>
2022-09-16 21:38:01 +0300

    Implement UserRepository, add CancellationToken to method parameters

--- MKeeper.DataAccess.PSQL/Repositories/UserRepository.cs
+++ MKeeper.DataAccess.PSQL/Repositories/UserRepository.cs
@@ -1,41 +1,67 @@
 ﻿using MKeeper.Domain.Models;
 using MKeeper.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
 
 namespace MKeeper.DataAccess.PSQL.Repositories;
 
 public class UserRepository : IUserRepository
 {
     private readonly MKeeperDbContext _context;
+    private readonly IMapper _mapper;
 
-    public UserRepository(MKeeperDbContext context) => _context = context;
-
-    public async Task<User> Get(int userId)
+    public UserRepository(MKeeperDbContext context, IMapper mapper)
     {
-        throw new NotImplementedException();
+        _context = context;
+        _mapper = mapper;
     }
 
-    public async Task<User> Get(string email)
+    public async Task<User> Get(int userId, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var userEntity = await _context.Users
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Id == userId, cancellationToken);
+        var result = _mapper.Map<User>(userEntity);
+        return result;
     }
 
-    public async Task<User[]> GetAll()
+    public async Task<User> Get(string email, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var userEntity = await _context.Users
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Email == email, cancellationToken);
+        var result = _mapper.Map<User>(userEntity);
+        return result;
     }
 
-    public async Task<int> Add(User user)
+    public async Task<User[]> GetAll(CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var userEntities = await _context.Users
+            .AsNoTracking()
+            .ToArrayAsync(cancellationToken);
+        var result = _mapper.Map<User[]>(userEntities);
+        return result;
     }
 
-    public async Task Update(User user)
+    public async Task<int> Add(User user, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var userEntity = _mapper.Map<Entities.User>(user);
+        await _context.Users.AddAsync(userEntity, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+        return userEntity.Id;
     }
 
-    public async Task Delete(int userId)
+    public async Task Update(User user, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var userEntity = _mapper.Map<Entities.User>(user);
+        _context.Users.Update(userEntity);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task Delete(int userId, CancellationToken cancellationToken = default)
+    {
+        var userEntity = new Entities.User() { Id = userId };
+        _context.Users.Remove(userEntity);
+        await _context.SaveChangesAsync();
     }
 }
--- MKeeper.Domain/Repositories/IUserRepository.cs
+++ MKeeper.Domain/Repositories/IUserRepository.cs
@@ -4,15 +4,15 @@ namespace MKeeper.Domain.Repositories;
 
 public interface IUserRepository
 {
-    Task<User> Get(int userId);
+    Task<User> Get(int userId, CancellationToken cancellationToken = default);
 
-    Task<User> Get(string email);
+    Task<User> Get(string email, CancellationToken cancellationToken = default);
 
-    Task<User[]> GetAll();
+    Task<User[]> GetAll(CancellationToken cancellationToken = default);
 
-    Task<int> Add(User user);
+    Task<int> Add(User user, CancellationToken cancellationToken = default);
 
-    Task Update(User user);
+    Task Update(User user, CancellationToken cancellationToken = default);
 
-    Task Delete(int userId);
+    Task Delete(int userId, CancellationToken cancellationToken = default);
 }
